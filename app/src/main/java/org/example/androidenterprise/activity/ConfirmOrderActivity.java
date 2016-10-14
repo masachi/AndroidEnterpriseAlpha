c237e44828b0202672e853316fb82443f31d2e7c
baiqingyu <553638690@qq.com>
2016-10-14 16:51:43 +0800

    Merge branch 'master' of D:\AndroidEnterpriseAlpha with conflicts.

--- app/src/main/java/org/example/androidenterprise/activity/ConfirmOrderActivity.java
+++ app/src/main/java/org/example/androidenterprise/activity/ConfirmOrderActivity.java
@@ -3,7 +3,7 @@ package org.example.androidenterprise.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.view.WindowManager;
+import android.widget.TextView;
 import org.example.androidenterprise.R;
 
 /**
@@ -11,10 +11,13 @@ import org.example.androidenterprise.R;
  */
 public class ConfirmOrderActivity extends Activity {
 
+    private TextView buyerLeaveMessageTv;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
         setContentView(R.layout.activity_confirm_order);
+        buyerLeaveMessageTv=(TextView)findViewById(R.id.buyer_leave_message_tv);
+        //TODO: click on this textview pop_up message board
     }
 }
