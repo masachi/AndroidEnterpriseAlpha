9a17b60ce7e2ad4d4c0f4a02f5fad4fe9601f4bd
byyoung1 <yxb3130@163.com>
2016-10-14 17:57:19 +0800

    commited by yangxuebing:                  课程日历UI                  已完成 ：课程日历UI的title制作、自定义日历控件                  未完成：日历底部的listview

---
+++ app/src/main/java/org/example/androidenterprise/activity/CourseCalendarActivity.java
@@ -0,0 +1,108 @@
+package org.example.androidenterprise.activity;
+
+/**
+ * Created by Administrator on 2016/10/14.
+ */
+
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+import org.example.androidenterprise.R;
+import org.example.androidenterprise.utils.EventDecorator;
+import org.example.androidenterprise.utils.MySelectorDecorator;
+import org.example.androidenterprise.utils.OneDayDecorator;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.concurrent.Executors;
+
+public class CourseCalendarActivity extends AppCompatActivity implements OnDateSelectedListener {
+
+    private MaterialCalendarView calendar_mv;
+
+    private final OneDayDecorator oneDayDecorator = new OneDayDecorator();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_calendar);
+        calendar_mv = (MaterialCalendarView) findViewById(R.id.calendar_mv);
+
+        calendar_mv.setOnDateChangedListener(this);
+        calendar_mv.setShowOtherDates(MaterialCalendarView.SHOW_ALL);
+
+        Calendar instance = Calendar.getInstance();
+        calendar_mv.setSelectedDate(instance.getTime());
+
+        Calendar instance1 = Calendar.getInstance();
+        instance1.set(instance1.get(Calendar.YEAR), Calendar.JANUARY, 1);
+
+        Calendar instance2 = Calendar.getInstance();
+        instance2.set(instance2.get(Calendar.YEAR), Calendar.DECEMBER, 31);
+
+        calendar_mv.state().edit()
+                .setMinimumDate(instance1.getTime())
+                .setMaximumDate(instance2.getTime())
+                .commit();
+
+        calendar_mv.addDecorators(
+                new MySelectorDecorator(this),
+                //new HighlightWeekendsDecorator(),
+                oneDayDecorator
+        );
+
+        new ApiSimulator().executeOnExecutor(Executors.newSingleThreadExecutor());
+    }
+
+    @Override
+    public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+        //If you change a decorate, you need to invalidate decorators
+        oneDayDecorator.setDate(date.getDate());
+        widget.invalidateDecorators();
+    }
+
+    /**
+     * Simulate an API call to show how to add decorators
+     */
+    private class ApiSimulator extends AsyncTask<Void, Void, List<CalendarDay>> {
+
+        @Override
+        protected List<CalendarDay> doInBackground(@NonNull Void... voids) {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Calendar calendar = Calendar.getInstance();
+            calendar.add(Calendar.MONTH, -2);
+            ArrayList<CalendarDay> dates = new ArrayList<>();
+            for (int i = 0; i < 30; i++) {
+                CalendarDay day = CalendarDay.from(calendar);
+                dates.add(day);
+                calendar.add(Calendar.DATE, 5);
+            }
+
+            return dates;
+        }
+
+        @Override
+        protected void onPostExecute(@NonNull List<CalendarDay> calendarDays) {
+            super.onPostExecute(calendarDays);
+
+            if (isFinishing()) {
+                return;
+            }
+
+            calendar_mv.addDecorator(new EventDecorator(Color.rgb(151, 200, 205), calendarDays));
+        }
+    }
+
+
+}
---
+++ app/src/main/java/org/example/androidenterprise/utils/MySelectorDecorator.java
@@ -0,0 +1,32 @@
+package org.example.androidenterprise.utils;
+
+import android.app.Activity;
+import android.graphics.drawable.Drawable;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
+import com.prolificinteractive.materialcalendarview.DayViewFacade;
+import org.example.androidenterprise.R;
+//import com.prolificinteractive.materialcalendarview.sample.R;
+
+/**
+ * Use a custom selector
+ */
+public class MySelectorDecorator implements DayViewDecorator {
+
+    private final Drawable drawable;
+
+    public MySelectorDecorator(Activity context) {
+        drawable = context.getResources().getDrawable(R.drawable.calendar_selector);
+    }
+
+    @Override
+    public boolean shouldDecorate(CalendarDay day) {
+        return true;
+    }
+
+    @Override
+    public void decorate(DayViewFacade view) {
+        view.setSelectionDrawable(drawable);
+    }
+}
