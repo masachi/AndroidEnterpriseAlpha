b4af4f8e88006e1b69caaa6fce267b4636bfa978
caishuang0217 <895531538@qq.com>
2016-10-15 17:17:20 +0800

    committed by caishuang : 学生反馈页面制作中

---
+++ app/src/main/java/org/example/androidenterprise/activity/StudentFeedBack.java
@@ -0,0 +1,14 @@
+package org.example.androidenterprise.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import org.example.androidenterprise.R;
+
+public class StudentFeedBack extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_students_feed_back);
+    }
+}
