9fe7ed0f771f87894b3b53fe26d4d486afdffe27
masachi chou <zhangjb42@gmail.com>
2016-10-30 13:12:16 +0800

    committed by masachi : update url address and portrait.

--- app/src/main/java/org/example/androidenterprise/activity/PortraitActivity.java
+++ app/src/main/java/org/example/androidenterprise/activity/PortraitActivity.java
@@ -1,14 +1,22 @@
 package org.example.androidenterprise.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import org.example.androidenterprise.R;
+import org.example.androidenterprise.utils.com.lling.photopicker.PhotoPickerActivity;
+import org.example.androidenterprise.utils.com.lling.photopicker.utils.ImageLoader;
+import org.example.androidenterprise.utils.com.lling.photopicker.utils.OtherUtils;
 import org.xutils.view.annotation.ContentView;
 import org.xutils.view.annotation.Event;
 import org.xutils.view.annotation.ViewInject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @ContentView(R.layout.activity_portrait)
 
@@ -20,10 +28,17 @@ public class PortraitActivity extends BaseActivity {
     TextView acceptBtn;
     @ViewInject(R.id.btn_change_portrait)
     Button changeBtn;
+    @ViewInject(R.id.img_head_portrait)
+    ImageView headportraitIv;
+
+    private List<String> mResults;
+    private int mColumnWidth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        int screenWidth = OtherUtils.getWidthInPx(getApplicationContext());
+        mColumnWidth = (screenWidth - OtherUtils.dip2px(getApplicationContext(), 4)) / 3;
     }
 
     @Event(value = {R.id.iv_return, R.id.tv_accept, R.id.btn_change_portrait})
@@ -36,8 +51,34 @@ public class PortraitActivity extends BaseActivity {
                 finish();
                 break;
             case R.id.btn_change_portrait:
-                
+                Intent intent = new Intent(this, PhotoPickerActivity.class);
+                intent.putExtra(PhotoPickerActivity.EXTRA_SHOW_CAMERA, true);
+                intent.putExtra(PhotoPickerActivity.EXTRA_SELECT_MODE, PhotoPickerActivity.MODE_SINGLE);
+                intent.putExtra(PhotoPickerActivity.EXTRA_MAX_MUN, PhotoPickerActivity.DEFAULT_NUM);
+                startActivityForResult(intent, 1);
                 break;
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                ArrayList<String> result = data.getStringArrayListExtra(PhotoPickerActivity.KEY_RESULT);
+                showResult(result);
+            }
+        }
+    }
+
+    private void showResult(ArrayList<String> paths) {
+        if (mResults == null) {
+            mResults = new ArrayList<>();
+        }
+        mResults.clear();
+        mResults.addAll(paths);
+        String headPortrait = mResults.get(0);
+        ImageLoader.getInstance().display(paths.get(0), headportraitIv, mColumnWidth, mColumnWidth);
+
     }
 }
--- app/src/main/java/org/example/androidenterprise/utils/UrlAddress.java
+++ app/src/main/java/org/example/androidenterprise/utils/UrlAddress.java
@@ -4,11 +4,12 @@ package org.example.androidenterprise.utils;
  * Created by sdlds on 2016/10/26.
  */
 public class UrlAddress {
-    public static String VIEWPAGER_URL = "http://128.199.201.13:8080/regist/ss";
-    public static String COURSE_LIST_URL = "http://128.199.201.13:8080/regist/sc";
-    public static String LOGIN_URL = "http://128.199.201.13:8080/login/api_login";
-    public static String REGISTER_URL = "http://128.199.201.13:8080/regist/getdata";
-    public static String COURSE_INFO_URL = "http://128.199.201.13:8080/music/api_classdetail";
-    public static String INSTRUMENT_DETAIL_URL = "http://128.199.201.13:8080/music/api_insdetail";
+    private static String URL_HEAD = "http://128.199.137.227:8080";
+    public static String VIEWPAGER_URL = URL_HEAD + "/myapi/showslide/api_showslide";
+    public static String COURSE_LIST_URL = URL_HEAD + "/regist/sc";
+    public static String LOGIN_URL = URL_HEAD + "/login/api_login";
+    public static String REGISTER_URL = URL_HEAD + "/myapi/regist/api_regist";
+    public static String COURSE_INFO_URL = URL_HEAD + "/music/api_classdetail";
+    public static String INSTRUMENT_DETAIL_URL = URL_HEAD + "/music/api_insdetail";
 
 }
